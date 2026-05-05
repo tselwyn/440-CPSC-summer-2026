208a64513d956e6ea3293505f62ad3464f611810
Tyler <tylerselwyn427@gmail.com>
2026-05-04 22:32:29 -0400

    added while loop logic, with keys for moving around. Goal is to have green circle that moves where I want it to go based on key I click

--- test/lab2Actual/Lab2.cpp
+++ test/lab2Actual/Lab2.cpp
@@ -17,4 +17,56 @@ int main() {
     bool done = false;
     ALLEGRO_EVENT ev;
 
+
+    while (!done) {
+        al_wait_for_event(event_queue, &ev);
+
+        if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+            switch (ev.keyboard.keycode) {
+            case ALLEGRO_KEY_UP:
+                pos_y -= 10;
+                direction = 0;
+                break;
+            case ALLEGRO_KEY_DOWN:
+                pos_y += 10;
+                direction = 1;
+                break;
+            case ALLEGRO_KEY_RIGHT:
+                pos_x += 10;
+                direction = 2;
+                break;
+            case ALLEGRO_KEY_LEFT:
+                pos_x -= 10;
+                direction = 3;
+                break;
+            case ALLEGRO_KEY_U:
+                pos_x -= 10;
+                pos_y -= 10;
+                direction = 4;
+                break;
+            case ALLEGRO_KEY_D:
+                pos_x += 10;
+                pos_y += 10;
+                direction = 5;
+                break;
+            case ALLEGRO_KEY_R:
+                pos_x += 10;
+                pos_y -= 10;
+                direction = 6;
+                break;
+            case ALLEGRO_KEY_L:
+                pos_x -= 10;
+                pos_y += 10;
+                direction = 7;
+                break;
+            case ALLEGRO_KEY_ESCAPE:
+                done = true;
+                break;
+            }
+        }
+
+        al_clear_to_color(al_map_rgb(0, 0, 0));
+        al_draw_filled_circle(pos_x, pos_y, 20, al_map_rgb(0, 255, 0));
+
+
   
